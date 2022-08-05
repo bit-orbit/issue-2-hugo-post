715c06aa3d9cda8913c212818a21bd85a61320e5
shabane <m.mohamadshabane@gmail.com>
2022-08-05 17:27:50 +0430

    [feat](create an object of an issue):
        create an object of an issue

---
+++ core.py
@@ -0,0 +1,25 @@
+class Issue():
+    def __init__(self, issue: dict) -> None:
+        self.issue = issue
+        self.url = None
+        self.title = None
+        self.user = None
+        self.labels = []
+        self.state = None
+        self.created_at = None
+        self.body = None
+
+    def get_issue_context(self):
+        self.url = self.issue.get('url')
+        self.title = self.issue.get('title')
+        self.user = self.issue.get('user').get('login')
+
+        for label in self.issue.get('labels'):
+            self.labels.append(label.get('name'))
+
+        self.state = self.issue.get('state')
+        self.created_at = self.issue.get('created_at')
+        self.body = self.issue.get('body')
+
+    def __str__(self) -> str:
+        return self.title
